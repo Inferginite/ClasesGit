789e449f5f85f36a3f268e457a1cd87f1d0e9959
Aldo Rodríguez <armyzrdz@hotmail.com>
2016-10-21 10:06:34 -0500

    feature: Aldo's view

--- Pruebasss/Front/views.py
+++ Pruebasss/Front/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from Front.models import UsuariosUaq
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 
@@ -18,14 +19,14 @@ def VistaPrueba2(request):
     contexto['PromedioGeneral']=promedioGeneral
     contexto['usersFuture']=UsuariosUaq.objects.filter(promedio__gte=8.0)
     contexto['usersNoFuture']=UsuariosUaq.objects.filter(promedio__lte=8.0)
-    
+
     contexto['NoHope']=UsuariosUaq.objects.filter(dado_de_bajo=True)
-    
+
     return render(request, 'Front/prueba.html',contexto)
 
 def JoseCruzEstefaniaAlmanza(request):
     contexto={}
-    
+
     try:
         alumnos=UsuariosUaq.objects.all()
         sumatotal=0
@@ -35,9 +36,27 @@ def JoseCruzEstefaniaAlmanza(request):
         contexto['PromedioGeneral']=promedioGeneral
         contexto['usersFuture']=UsuariosUaq.objects.filter(promedio__gte=8.0)
         contexto['usersNoFuture']=UsuariosUaq.objects.filter(promedio__lte=8.0)
-        
+
         contexto['NoHope']=UsuariosUaq.objects.filter(dado_de_bajo=True)
     except Exception, e:
         return render(request, 'Front/JoseCruzEstefaniaAlmanza.html',contexto)
 
     return render(request, 'Front/JoseCruzEstefaniaAlmanza.html',contexto)
+
+class AldoRodriguezMoralesView(TemplateView):
+
+    template_name = "AldoRodriguezMorales.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(AldoRodriguezMoralesView, self).get_context_data(**kwargs)
+        alumnos=UsuariosUaq.objects.all()
+        sumatotal=0
+        for alumno in alumnos:
+            sumatotal=sumatotal+alumno.promedio
+        promedioGeneral=sumatotal/alumnos.count()
+        context['PromedioGeneral']=promedioGeneral
+        context['usersFuture']=UsuariosUaq.objects.filter(promedio__gte=8.0)
+        context['usersNoFuture']=UsuariosUaq.objects.filter(promedio__lte=8.0)
+
+        context['NoHope']=UsuariosUaq.objects.filter(dado_de_bajo=True)
+        return context
